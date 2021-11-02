f31a086d08b568d8f18aabf8da82e36f7648d1ba
Pavel Kozhaev <zxc321z9@gmail.com>
2021-11-03 00:06:10 +0300

    Фикс
    Переопределить метод toString в классе Dog
    Не создавать новую сущность Timer

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -15,7 +15,6 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
-    @Bean
     public Timer getTimer() {
         return timer;
     }
@@ -24,8 +23,6 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
-        System.out.println("bean At:");
         System.out.println(timer.getTime());
         System.out.println("________________________");
     }
